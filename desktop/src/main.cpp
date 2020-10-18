e2c37a925ab834b0aa37c92ef471e54c7740f549
Oleksandr Koreniak <alexk2002g@gmail.com>
2020-10-18 18:51:17 +0300

    initil release version

---
+++ desktop/src/main.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Damir Porobic <https://github.com/damirporobic>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#if  defined(_WIN32) && defined(QT_NO_DEBUG)
+// Prevent starting console in background under windows
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+
+#include <QApplication>
+
+#include "BuildConfig.h"
+#include "src/bootstrapper/BootstrapperFactory.h"
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+    qDebug() << app.applicationDirPath() << " " << app.applicationFilePath();
+    app.setOrganizationName(QStringLiteral("ksnip"));
+    app.setOrganizationDomain(QStringLiteral("ksnip.ksnip.org"));
+    app.setApplicationName(QStringLiteral("ksnip"));
+    app.setApplicationVersion(QStringLiteral(KSNIP_VERSION));
+    app.setDesktopFileName(QStringLiteral("org.ksnip.ksnip.desktop"));
+
+    app.setStyle(KsnipConfigProvider::instance()->applicationStyle());
+
+	BootstrapperFactory bootstrapperFactory;
+	return bootstrapperFactory.create()->start(app);
+}
